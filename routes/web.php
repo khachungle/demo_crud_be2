df92fbfbcb0cfcf3bc553ed0ca6edaa063e05485
Le Khac Hung <khachung.bb@gmail.com>
2024-04-12 15:22:05 +0700

    Chinh sua route

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,3 @@ Route::get('users/{id}', [UserController::class, 'show'])->name('users.show');
 Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get('/', [UserController::class, 'index'])->name('users.index');
-Route::get('create', [UserController::class, 'create'])->name('users.create');
-Route::post('create', [UserController::class, 'store'])->name('users.store');
